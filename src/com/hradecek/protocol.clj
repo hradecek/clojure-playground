08eddf92bf0cd9dc4d9336eeeb28f1f4ee04b77e
Ivo Hradek <ihradek@redhat.com>
2017-02-25 22:22:15 +0100

    java.util.List protocol implementation

--- src/com/hradecek/protocol.clj
+++ src/com/hradecek/protocol.clj
@@ -8,7 +8,14 @@
   (cat' [this other]
     (.concat this other)))
 
+(extend-type java.util.List
+  Concatenatable
+  (cat' [this other]
+    (concat this other)))
+
 (defn -main []
   (println
-    (cat' "House" " of Wolves")))
+    (cat' "House" " of Wolves"))
+  (println
+    (cat' [1 2 3] [4 5 6 7])))
 
